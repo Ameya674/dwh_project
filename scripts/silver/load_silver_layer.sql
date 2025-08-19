7cc2cf11b759e6ace28492b96009ad0e780bd028
Ameya Gurjar <76804848+Ameya674@users.noreply.github.com>
2025-08-19 16:44:17 -0700

    Update load_silver_layer.sql

--- scripts/silver/load_silver_layer.sql
+++ scripts/silver/load_silver_layer.sql
@@ -15,12 +15,63 @@ BEGIN
 
     SET @batch_start_time = GETDATE();
     PRINT '======================================';
-		PRINT 'Loading Silver Layer';
-		PRINT '======================================';
+	PRINT 'Loading Silver Layer';
+	PRINT '======================================';
 
-		PRINT '--------------------------------------';
-		PRINT 'Load CRM Tables';
-		PRINT '--------------------------------------';
+	PRINT '--------------------------------------';
+	PRINT 'Load CRM Tables';
+	PRINT '--------------------------------------';
+
+
+	SET @start_time = GETDATE();
+	PRINT 'Truncating Table: silver.crm_cust_info';
+	TRUNCATE TABLE silver.crm_cust_info;
+	PRINT 'Inserting Data into silver.crm_cust_info';
+	INSERT INTO silver.crm_cust_info 
+	(
+		cst_id, 
+		cst_key, 
+		cst_firstname, 
+		cst_lastname, 
+		cst_marital_status, 
+		cst_gndr,
+		cst_create_date
+	)
+	SELECT
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname,
+	TRIM(cst_lastname) AS cst_lastname,
+	CASE
+		WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		ELSE 'n/a'
+	END AS cst_marital_status,
+	CASE
+		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		WHEN UPPER(TRIM(cst_gndr)) = 'F' Then 'Female'
+		ELSE 'n/a'
+	END AS cst_gndr,
+	cst_create_date
+	FROM 
+	(
+		SELECT *,
+		ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag
+		FROM bronze.crm_cust_info
+		WHERE cst_id IS NOT NULL
+	)t
+	WHERE flag = 1 
+	SET @end_date = GETDATE();
+	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+	PRINT '>> -----------------------------------';
+
+
+	SET @start_time = GETDATE();
+	PRINT 'Truncating Table silver.crm_prd_info';
+	TRUNCATE TABLE silver.crm_prd_info;
+	PRINT 'Inserting Data into silver.crm_prd_info';
+	
+
 
     
   END TRY
